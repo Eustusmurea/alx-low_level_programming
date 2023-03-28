e28e02525a00b5c62e60e75995f7c604442ef97f
Eustusmurea <60316624+Eustusmurea@users.noreply.github.com>
2023-03-28 15:47:47 +0300

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,46 +1,24 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
  */
-int _atoi(char *s)
+char *_strcpy(char *dest, char *src)
 {
-int i, d, n, len, f, digit;
+int l = 0;
+int x = 0;
 
-i = 0;
-d = 0;
-n = 0;
-len = 0;
-f = 0;
-digit = 0;
-
-while (s[len] != '\0')
-len++;
-
-while (i < len && f == 0)
+while (*(src + l) != '\0')
 {
-if (s[i] == '-')
-++d;
-
-if (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-if (d % 2)
-digit = -digit;
-n = n * 10 + digit;
-f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
-break;
-f = 0;
+l++;
 }
-i++;
+for ( ; x < l ; x++)
+{
+dest[x] = src[x];
 }
-
-if (f == 0)
-return (0);
-
-return (n);
+dest[l] = '\0';
+return (dest);
 }
