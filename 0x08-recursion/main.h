3266d3db94c6c114464813f12b8a76e19c3ea0d2
Eustusmurea <60316624+Eustusmurea@users.noreply.github.com>
2023-04-05 16:33:33 +0300

    Header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,29 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/// @brief 
-/// @param s 
 void _puts_recursion(char *s);
 int _putchar(char c);
-/// @brief 
-/// @param s 
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-/// @brief 
-/// @param n 
-/// @return 
 int factorial(int n);
 int _pow_recursion(int x, int y);
-/// @brief 
-/// @param n 
-/// @return 
 int _sqrt_recursion(int n);
-/// @brief 
-/// @param n 
-/// @return 
 int is_prime_number(int n);
-/// @brief 
-/// @param s 
-/// @return 
 int is_palindrome(char *s);
 #endif
