e0c4ac3b67c74482ed97c56aef3399df85397f62
Eustusmurea <60316624+Eustusmurea@users.noreply.github.com>
2023-04-05 15:59:18 +0300

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/// @brief 
+/// @param x 
+/// @param y 
+/// @return 
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
