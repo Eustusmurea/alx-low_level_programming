9ee0e244233019ee0eec5769e30c3099c060f8ef
Eustusmurea <60316624+Eustusmurea@users.noreply.github.com>
2023-04-05 17:02:35 +0300

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,10 @@
 #include "main.h"
-/// @brief 
-/// @param s 
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+ 
 void _puts_recursion(char *s)
 {
  if (*s)
