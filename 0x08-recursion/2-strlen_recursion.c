2509ee6f4975696401b138e3910b9ac873e13b93
Eustusmurea <60316624+Eustusmurea@users.noreply.github.com>
2023-04-05 15:41:43 +0300

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/// @brief 
+/// @param s 
+/// @return 
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+
+	return (longit);
+}
