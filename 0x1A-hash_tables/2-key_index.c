1c4887d983b873195c4ac06f8ce00340110ab241
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2023-09-12 10:29:56 +0300

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,13 @@
 #include "hash_tables.h"
+
 /**
-* key_index - sets a hash table
-* @key: the key
-* @size: the value for size
-* Return: where the key/value is stored in the array
-*/
+ * key_index - get index for key given
+ * @key: key (string) to hash
+ * @size: size of hash table
+ *
+ * Return: index of 'key' using djb2
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index;
-
-	index = hash_djb2(key) % size;
-	return (index);
+	return (hash_djb2(key) % size);
 }
