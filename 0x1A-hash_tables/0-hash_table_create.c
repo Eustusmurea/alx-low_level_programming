346ad3e51a31e31b4081aff22d757fbd1c7cd641
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2023-09-12 10:21:47 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,25 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
-/**
-* hash_table_create - creates a hash table
-*
-* @size: the size of the table
-* Return: returns the table, or NULL on fail
-*/
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *table = NULL;
 
-	if (size == 0)
-		return (NULL);
-	table = malloc(sizeof(hash_table_t));
-	if (!table)
-		return (NULL);
-	(*table).size = size;
-	(*table).array = malloc(sizeof(hash_node_t *) * size);
-	if (!(*table).array)
-	{
-		free(table);
-		return (NULL);
-	}
-	return (table);
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
 }
