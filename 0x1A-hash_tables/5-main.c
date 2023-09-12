ba504f38cf507a3e05672b6f607bc4f98c93f06a
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2023-09-12 11:19:33 +0300

    Update 5-main.c

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -4,7 +4,7 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
@@ -16,10 +16,10 @@ int main(void)
     hash_table_print(ht);
     hash_table_set(ht, "c", "fun");
     hash_table_set(ht, "python", "awesome");
-    hash_table_set(ht, "Jennie", "and Jay love asm");
+    hash_table_set(ht, "Bob", "and Kris love asm");
     hash_table_set(ht, "N", "queens");
     hash_table_set(ht, "Asterix", "Obelix");
-    hash_table_set(ht, "Betty", "Holberton");
+    hash_table_set(ht, "Betty", "Cool");
     hash_table_set(ht, "98", "Battery Street");
     hash_table_print(ht);
     return (EXIT_SUCCESS);
